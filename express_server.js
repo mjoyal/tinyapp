3c45d985289e2250a96e140b6d24c12672ac7e4e
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-09-29 19:02:41 +0000

    added form to POST to /urls, added shortURL & longURL value pair to the urlDatabase

--- express_server.js
+++ express_server.js
@@ -8,37 +8,7 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }; 
-
-app.set("view engine", "ejs"); 
-
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  console.log(templateVars); 
-  res.render('./urls_index', templateVars); 
-});
-
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; 
-  console.log(templateVars); 
-  res.render('./urls_short', templateVars); 
-});
-
-app.post('/urls', (req, res) => {
-  console.log(req.body); 
-  res.send('OK'); 
-});
-
-function generateRandomString () {
+const generateRandomString = function () {
   let random = ''; 
   const options = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; 
   for(let i = 0; i < 6; i++) {
@@ -47,9 +17,46 @@ function generateRandomString () {
   return random; 
 }; 
 
-console.log(generateRandomString()); 
+app.set("view engine", "ejs"); 
 
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
+//render urls_new.ejs - show a POST form with one input (for the long URL) and button
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// render urls_index - show a table of all the long & short URLS
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('./urls_index', templateVars); 
+});
+
+// render urls_short - a page with the individual tiny URL id and corresponding long URL, the tiny URL id is a link to the long URL site
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; 
+  res.render('./urls_short', templateVars); 
+});
+
+// calls generateRandomString() to make a short URL, adds the short URL as a key to the urlDatabase 
+// (uses body parser to make the info into object)
+// posts the form information from urls/new (urls_new) to the /urls (urls_index)
+
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString(); 
+  urlDatabase[shortURL] = req.body.longURL; 
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// tells the server to redirect to the long URL when routed to /u/:shortURL (link setup in urls_short)
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]; 
+  res.redirect(longURL);
+});
+
+// asks the server to listen for requests
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`); 
 }); 
