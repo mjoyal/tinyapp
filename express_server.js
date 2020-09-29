8e0104604021f465535ed5adb0f36a85d2cfc43d
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-09-29 22:24:51 +0000

    added login / logout functionality with cookies

--- express_server.js
+++ express_server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express(); 
 const PORT = 8080; 
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser'); 
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs"); 
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com", 
   "DZtoes": "http://www.facebook.com", 
 }; 
+
 const generateRandomString = function () {
   let random = ''; 
   const options = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; 
@@ -18,7 +22,6 @@ const generateRandomString = function () {
   return random; 
 }; 
 
-app.set("view engine", "ejs"); 
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
@@ -26,18 +29,33 @@ app.set("view engine", "ejs");
 
 //render urls_new.ejs - show a POST form with one input (for the long URL) and button
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies['username']};
+  res.render("urls_new", templateVars);
 });
 
 // render urls_index - show a table of all the long & short URLS
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies['username']};
   res.render('./urls_index', templateVars); 
 });
 
+// allows users to logout (clears username cookie)
+app.post('/urls/login', (req, res) => {
+  const username = res.cookie('username', req.body.username); 
+  res.redirect(`/urls/`);
+}); 
+
+// allows users to setup a username 
+app.post('/urls/logout', (req, res) => {
+  res.clearCookie('username'); 
+  res.redirect(`/urls/`);
+}); 
+
+
+
 // render urls_short - a page with the individual tiny URL id and corresponding long URL, the tiny URL id is a link to the long URL site
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; 
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username']}; 
   res.render('./urls_show', templateVars); 
 });
 
@@ -50,6 +68,8 @@ app.post('/urls', (req, res) => {
   urlDatabase[shortURL] = req.body.longURL; 
   res.redirect(`/urls/${shortURL}`);
 });
+
+
 
 // tells the server to redirect to the long URL when routed to /u/:shortURL (link setup in urls_short)
 app.get("/u/:shortURL", (req, res) => {
@@ -64,14 +84,15 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect(`/urls/`);
 });
 
+// allow users to edit their longURL
 app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL; 
   const newlongURL = req.body.editedURL; 
-  console.log('shortURL:', shortURL); 
-  console.log('newLongURL:', newlongURL);
   urlDatabase[shortURL] = newlongURL; 
   res.redirect(`/urls/`);
 });
+
+
 
 // asks the server to listen for requests
 app.listen(PORT, () => {
