70677e065e11ca97218af20f373a81a38574f5ce
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-09-29 03:09:19 +0000

    installed package to handle decoding buffer, added random string generating function

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const app = express(); 
 const PORT = 8080; 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,12 +11,13 @@ const urlDatabase = {
 
 app.set("view engine", "ejs"); 
 
-app.get('/', (req, res) => {
-  res.send('Hello!'); 
-}); 
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
@@ -23,15 +26,29 @@ app.get("/urls", (req, res) => {
   res.render('./urls_index', templateVars); 
 });
 
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; 
   console.log(templateVars); 
   res.render('./urls_short', templateVars); 
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post('/urls', (req, res) => {
+  console.log(req.body); 
+  res.send('OK'); 
 });
+
+function generateRandomString () {
+  let random = ''; 
+  const options = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; 
+  for(let i = 0; i < 6; i++) {
+    random += options.charAt(Math.floor(Math.random() * options.length)); 
+  }
+  return random; 
+}; 
+
+console.log(generateRandomString()); 
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`); 
