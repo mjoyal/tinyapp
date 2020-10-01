a878f191fbc8d4bc959a8de45e31142a8e73290e
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-10-01 19:28:26 +0000

    ensured that users can only delete / edit their own links

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "aJ48lW"},
-  "idk768": {longURL: "http://www.google.com", userID: "idk768"}, 
+  "idk768": {longURL: "http://www.google.com", userID: "DZtoes"}, 
 }; 
 
 const users = {
@@ -42,12 +42,23 @@ const checkEmail = function (emailToCheck) {
 }
 
 const checkPassword = function (userid, passwordInput) {
-
   if(users[userid].password === passwordInput) {
     return true; 
   }
   return false; 
 }; 
+
+const urlsForUser = (currentUserID, database) =>  {
+  const userURLs = {}; 
+  for(const url in database) {
+    console.log('userID:', database[url].userID );
+    if(database[url].userID === currentUserID) {
+      userURLs[url] = database[url].longURL; 
+    }
+  }
+  return userURLs;
+}; 
+
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
@@ -55,16 +66,21 @@ const checkPassword = function (userid, passwordInput) {
 
 //render urls_new.ejs - show a POST form with one input (for the long URL) and button
 app.get("/urls/new", (req, res) => {
-  if(req.cookies['user_id'] === undefined); { 
+
+  if(req.cookies['user_id'] === undefined) { 
     res.redirect("/login"); 
+  } else {
+    const templateVars = {user: users[req.cookies['user_id']]};
+    res.render("urls_new", templateVars);
   }
-  const templateVars = {user: users[req.cookies['user_id']]};
-  res.render("urls_new", templateVars);
+
 });
 
 // render urls_index - show a table of all the long & short URLS
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: users[req.cookies['user_id']]};
+  const userID = req.cookies['user_id']; 
+  const userURLs = urlsForUser(userID, urlDatabase); 
+  const templateVars = { urls: userURLs, user: users[req.cookies['user_id']]};
   res.render('./urls_index', templateVars); 
 });
 
@@ -123,12 +139,9 @@ app.post('/logout', (req, res) => {
   res.redirect(`/urls/`);
 }); 
 
-
-
 // render urls_short - a page with the individual tiny URL id and corresponding long URL, the tiny URL id is a link to the long URL site
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.cookies['user_id']]}; 
-  console.log(urlDatabase[req.params.shortURL].longURL); 
   res.render('./urls_show', templateVars); 
 });
 
@@ -138,7 +151,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString(); 
-  urlDatabase[shortURL] = req.body.longURL; 
+  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.cookies['user_id']}; 
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -152,17 +165,30 @@ app.get("/u/:shortURL", (req, res) => {
 
 // Deletes individual shortURL/longURL pair from urlDatabase, redirects to /urls to show updates. 
 app.post('/urls/:shortURL/delete', (req, res) => {
+  const userID = req.cookies['user_id']; 
+  const userURLs = urlsForUser(userID, urlDatabase); 
   const shortURL = req.params.shortURL; 
-  delete urlDatabase[shortURL]
-  res.redirect(`/urls/`);
+  if(userURLs[shortURL]) {
+    delete urlDatabase[shortURL];
+    res.redirect(`/urls/`);
+  } else {
+    res.status('403').json({message: 'Cannot delete this short URL, belongs to another user'});
+  }
 });
 
 // allow users to edit their longURL
 app.post('/urls/:shortURL', (req, res) => {
+  const userID = req.cookies['user_id']; 
+  const userURLs = urlsForUser(userID, urlDatabase); 
   const shortURL = req.params.shortURL; 
-  const newlongURL = req.body.editedURL; 
-  urlDatabase[shortURL] = newlongURL; 
-  res.redirect(`/urls/`);
+  if(userURLs[shortURL]) {
+    const newlongURL = req.body.editedURL; 
+    urlDatabase[shortURL].longURL = newlongURL; 
+    res.redirect(`/urls/`);
+  } else {
+    res.status('403').json({message: 'Cannot edit this short URL, belongs to another user'});
+  }
+  
 });
 
 
